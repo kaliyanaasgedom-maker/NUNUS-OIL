efde0d9c970956d52fc4b0c84c3e603b84385075
kaliyanaasgedom-maker <kaliyanaasgedom@icloud.com>
2026-08-05 20:32:43 +0300

    Refactor sendOrder to include total price calculation
    Added total price calculation for the order.

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ window.onload = function() {
         button.onclick = function() {
             details.style.display = "block";
         };
-    }
-
-};
-function sendOrder() {
+    function sendOrder() {
 
 let name = document.getElementById("customerName").value;
 let phone = document.getElementById("phoneNumber").value;
 let address = document.getElementById("address").value;
 let quantity = document.getElementById("quantity").value;
 
+let total = quantity * 550;
+
 let message =
 "NUNU'S Hair Oil Order%0A%0A" +
 "Name: " + name + "%0A" +
 "Phone: " + phone + "%0A" +
 "Address: " + address + "%0A" +
-"Quantity: " + quantity;
+"Quantity: " + quantity + "%0A" +
+"Total Price: " + total + " ETB";
 
 window.open(
 "https://wa.me/251911626334?text=" + message,
